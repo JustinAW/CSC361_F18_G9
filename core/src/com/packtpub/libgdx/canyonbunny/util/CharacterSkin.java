4515e8b534d453f8e983f217864c18bbb79bfa01
Justin Study <js4050@ship.edu>
2018-09-29 10:00:14 -0400

    252 253 character skin code

---
+++ core/src/com/packtpub/libgdx/canyonbunny/util/CharacterSkin.java
@@ -0,0 +1,38 @@
+package com.packtpub.libgdx.canyonbunny.util;
+
+import com.badlogic.gdx.graphics.Color;
+
+/**
+ * @author Justin Study
+ *
+ * abstracts all selectable character skins
+ */
+public enum CharacterSkin
+{
+	//3 color options
+	WHITE("White", 1.0f, 1.0f, 1.0f),
+	GRAY("Gray", 0.7f, 0.7f, 0.7f),
+	BROWN("Brown", 0.7f, 0.5f, 0.3f);
+	
+	private String name;
+	private Color color = new Color();
+	
+	private CharacterSkin(String name, float r, float g, float b)
+	{
+		this.name = name;
+		color.set(r, g, b, 1.0f);
+	}
+	
+	//returns name of skin
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+	
+	//return color of skin
+	public Color getColor()
+	{
+		return color;
+	}
+}
