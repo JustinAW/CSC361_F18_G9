aa8b4ce1c0cf1eba65177b263c3017c4196cc4d6
Justin Study <js4050@ship.edu>
2018-09-29 07:58:22 -0400

    232-233 canyonbunnymain code

--- core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
+++ core/src/com/packtpub/libgdx/canyonbunny/CanyonBunnyMain.java
@@ -1,84 +1,29 @@
 package com.packtpub.libgdx.canyonbunny;
 
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.packtpub.libgdx.canyonbunny.game.WorldController;
-import com.packtpub.libgdx.canyonbunny.game.WorldRenderer;
 import com.badlogic.gdx.assets.AssetManager;
 import com.packtpub.libgdx.canyonbunny.game.Assets;
-
-
-public class CanyonBunnyMain implements ApplicationListener
+import com.packtpub.libgdx.canyonbunny.screens.MenuScreen;
+/**
+ * @author Justin Study ch. 7
+ * deleted old code and replaced with just the create method
+ */
+public class CanyonBunnyMain extends Game
 {
-	private static final String TAG =		
-		CanyonBunnyMain.class.getName();
-	
-	private WorldController worldController;
-	private WorldRenderer worldRenderer;
-	private boolean paused;
-	
-	@Override public void create () 
-	{ 
-		//Set Libgdx log level to DEBUG
+	//platform independent entry point for the game. LibGdx instructed to through 
+	//setScreen() method by Game class to change current screen. call menuscreen to 
+	//start the game
+	@Override
+	public void create()
+	{
+		//Set Libgdx log level
 		Gdx.app.setLogLevel(Application.LOG_DEBUG);
-		
-		//Load assets
+		//Load Assets
 		Assets.instance.init(new AssetManager());
-		
-		//Initialize controller and renderer
-		worldController = new WorldController();
-		worldRenderer = new WorldRenderer(worldController);
-		
-		//Game world is active on start
-		paused = false;
+		//Start game at menu screen
+		setScreen(new MenuScreen(this));
 	}
-	
-	@Override public void render () 
-	{
-		//Do not update game world when paused.
-		if(!paused) 
-		{
-			//Update game world by the time that has passed
-			//since last rendered frame.
-			worldController.update(Gdx.graphics.getDeltaTime());
-		}
-		
-		//  Update  game  world  by  the  time  that  has  passed
-		//  since  last  rendered  frame.
-		worldController.update(Gdx.graphics.getDeltaTime());
-		
-		//  Sets  the  clear  screen  color  to:  Cornflower  Blue
-		Gdx.gl.glClearColor(0x64/255.0f, 0x95/255.0f,  0xed/255.0f, 0xff/255.0f);
-		
-		//clears the screen
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		//render game world to screen
-		worldRenderer.render();
-	}
-	
-	@Override public void resize (int width, int height)
-	{
-		worldRenderer.resize(width, height);
-	}
-	
-	@Override public void pause () 
-	{
-		paused = true;
-	}
-	
-	@Override public void resume () 
-	{ 
-		paused = false;
-	}
-	
-	@Override public void dispose () 
-	{
-		worldRenderer.dispose();
-		Assets.instance.dispose();
-	}
-	
-	
 }
+
